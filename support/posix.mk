6936673a6873530c0927a5f923fd187391f8d06b
Jörg Schiller <js@direktzu.de>
2011-05-28 14:35:22 +0200

    Should habe looked first what opdenkamp did to fix the package building problem. Merged commit fb0a92ffb157aa682463.

--- support/posix.mk
+++ support/posix.mk
@@ -1,16 +1,22 @@
 
-prefix ?= $(INSTALLPREFIX)
-INSTBIN= $(prefix)/bin
-INSTMAN= $(prefix)/share/man1
+INSTBIN=  ${DESTDIR}${INSTALLPREFIX}/bin
+INSTMAN=  ${DESTDIR}${INSTALLPREFIX}/share/man1
+INSTDBG=  ${DESTDIR}${INSTALLPREFIX}/lib/debug/bin
 MAN=man/tvheadend.1
 
 install: ${PROG} ${MAN}
-	mkdir -p ${DESTDIR}$(INSTBIN)
-	install -s ${PROG} ${DESTDIR}$(INSTBIN)
+	mkdir -p ${INSTBIN}
+	install -T ${PROG} ${INSTBIN}/tvheadend
+	mkdir -p ${INSTMAN}
+	install ${MAN} ${INSTMAN}
 
-	mkdir -p ${DESTDIR}$(INSTMAN)
-	install ${MAN} ${DESTDIR}$(INSTMAN)
+install-debug: ${PROG}
+	mkdir -p ${INSTDBG}
+	objcopy --only-keep-debug ${INSTBIN}/tvheadend ${INSTDBG}/tvheadend.debug
+	strip -g ${INSTBIN}/tvheadend
+	objcopy --add-gnu-debuglink=${INSTDBG}/tvheadend.debug ${INSTBIN}/tvheadend
 
 uninstall:
-	rm -f ${DESTDIR}$(INSTBIN)/${PROG}
-	rm -f ${DESTDIR}$(INSTMAN)/${MAN}
+	rm -f ${INSTBIN}/tvheadend
+	rm -f ${INSTDBG}/tvheadend.debug
+	rm -f ${INSTMAN}/tvheadend.1
